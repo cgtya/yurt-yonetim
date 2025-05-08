2c2dbdf69dce585a46f35a440382218f3199391a
cgtya <cagataysln06@outlook.com>
2025-05-09 00:29:21 +0300

    öğrenci ekleme arayüzü güncellendi

--- src/main/java/org/corba/yurtyonetim/gui/StudentAdd.java
+++ src/main/java/org/corba/yurtyonetim/gui/StudentAdd.java
@@ -1,4 +1,26 @@
 package org.corba.yurtyonetim.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class StudentAdd extends BaseMenu {
+    @FXML
+    private TextField nameBox;
+    @FXML
+    private TextField surnameBox;
+    @FXML
+    private TextField mailBox;
+    @FXML
+    private TextField phoneBox;
+    @FXML
+    private TextField IDBox;
+    @FXML
+    private Label statusLabel;
+
+    public void buttonClickAddStudent(ActionEvent event) {
+        
+    }
+
 }
