18715b72dc05935dd65296664b1d4add7665572d
cgtya <cagataysln06@outlook.com>
2025-05-09 19:55:42 +0300

    linkte ufak bir değişiklik

--- src/main/java/org/corba/yurtyonetim/users/staticgecici.java
+++ src/main/java/org/corba/yurtyonetim/users/staticgecici.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.*;
 
 public class staticgecici {
-    private static final String url = "jdbc:mysql://localhost:3306/kullanicilar?useSSL=false&serverTimezone=UTC";
+    private static final String url = "jdbc:mysql://localhost:3306/kullanicilar?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
     private static final String user = "root";
     private static final String databasePassword = "Omer200526a";
 
