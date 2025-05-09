cc0b8b8354d6e43780258408810010430a2ab5ac
cgtya <cagataysln06@outlook.com>
2025-05-09 14:26:09 +0300

    student edit güncellendi

--- src/main/java/org/corba/yurtyonetim/gui/StudentEdit.java
+++ src/main/java/org/corba/yurtyonetim/gui/StudentEdit.java
@@ -1,4 +1,11 @@
 package org.corba.yurtyonetim.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class StudentEdit extends BaseMenu {
+    @FXML
+    private ChoiceBox<String> selectSearchAttribute;
+
+
 }
