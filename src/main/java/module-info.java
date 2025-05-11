90caa75c2ac605e837f18276d66b1480332ef436
cgtya <cagataysln06@outlook.com>
2025-05-11 12:40:11 +0300

    kullanılmayan kütüphaneler kaldırıldı

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module org.corba.yurtyonetim {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
