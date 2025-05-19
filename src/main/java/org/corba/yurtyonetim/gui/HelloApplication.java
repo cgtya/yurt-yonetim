0ee54ed8bb4a7ffd730d19ecd2b9ff04226150e5
cgtya <cagataysln06@outlook.com>
2025-05-20 00:48:12 +0300

    login pencere boyutu ayarlandı

--- src/main/java/org/corba/yurtyonetim/gui/HelloApplication.java
+++ src/main/java/org/corba/yurtyonetim/gui/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("KYK Yurt Yönetim Sistemi");
         stage.setScene(scene);
         stage.setMinHeight(480);
